25af51872635d50c6cd6c50a185429110d3c068b
Matias Landaburu <matilandaburu@gmail.com>
2022-03-28 16:36:27 -0300

    draw rectangle

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-
-import React, {useRef} from 'react'
+import React, {useEffect, useRef} from 'react'
 import './App.css';
 import * as facemesh  from "@tensorflow-models/facemesh"
 import Webcam from 'react-webcam';
 import { tf } from '@tensorflow/tfjs';
 import {drawMesh} from "./utilities.js"
-
-
+ 
+ 
+ 
 function App() {
   //setup references
   const webcamRef =  useRef(null);
   const canvasRef = useRef(null)
-
-
+  var ctx = null;
+ 
+ 
+ 
+  useEffect(() => {
+ 
+      ctx = canvasRef.current.getContext("2d")
+     console.log(ctx)
+     ctx.beginPath();
+     ctx.strokeStyle = "blue";
+     ctx.rect(0, 0, 100, 100);
+     ctx.stroke();
+  }, [])
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   // load facemesh
   const runFacemesh =  async() => {
+ 
     const net = await facemesh.load({
       inputResolution:{width:480, height:240}, scale:0.8
     })
     setInterval(() => {
+ 
       detect(net)
     }, 0)
-
+ 
   }
-
+ 
   // Detect function
   const detect =  async(net) => {
     if(typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4){
@@ -31,64 +55,77 @@ function App() {
       const video = webcamRef.current.video
       const videoWidth = webcamRef.current.video.videoWidth
       const videoHeight = webcamRef.current.video.videoHeight
-      
+ 
       // Set video width
       webcamRef.current.video.width = videoWidth
       webcamRef.current.video.height = videoHeight
-      console.log("webCamRef Width:" + webcamRef.current.video.width)
-      console.log("webCamRef Height:" + webcamRef.current.video.height)
-
+ 
       // set canvas width
       canvasRef.current.width = videoWidth
       canvasRef.current.height = videoHeight
-      console.log("canvas Width:" + canvasRef.current.width)
-      console.log("canvas Height:" + canvasRef.current.height )
-
+ 
       // Make detections
       const face = await net.estimateFaces(video);
       console.log(face)
-
+ 
       // Get canvas context for drawing
-
-      const ctx = canvasRef.current.getContext("2d")
-      
-      drawMesh(face, ctx)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     }
+ 
   }
-
+ 
   runFacemesh()
+ 
   return (
     <div className="App">
       <header className='App-header'>
         <div className='containter'>
           <div className='row'>
             <div className='col-12'>
-            <Webcam id='webCam' ref={webcamRef} style={ 
+            <Webcam ref={webcamRef} style={ 
         {
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
           zIndex:9,
-          marginLeft: "auto",
-          marginRight: "auto",
-          textAlign:"center"
-          
+          width:480,
+          height:240
+ 
         }
       }/>
-      <canvas id="canvas" ref={canvasRef} style={
+      <canvas ref={canvasRef} style={
         {
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
           zIndex:9,
-          marginLeft: "auto",
-          marginRight: "auto",
-          textAlign:"center"
+          width:480,
+          height:240
         }
       }/>
             </div>
           </div>
         </div>
-
+ 
       </header>
     </div>
   );
 }
-
-
-
+ 
+ 
+ 
 export default App;
--- src/utilities.js
+++ src/utilities.js
@@ -12,13 +12,19 @@ export const drawMesh = (predictions, ctx) => {
         predictions.forEach((prediction) => {
             const keyPoints = prediction.scaledMesh;
             for (let i = 0; i < keyPoints.length; i++) {
-                const x = keyPoints[i][0];
+                /*const x = keyPoints[i][0];
                 const y = keyPoints[i][1];
                 ctx.beginPath();
                 ctx.arc(x,y,1,0,3*Math.PI);
+                ctx.rect()    
                 ctx.fillStyle = "aqua"
                 ctx.fill();
-                
+                */
+
+                    const start = predictions[i].topLeft;
+                    const end = predictions[i].bottomRight;
+                    const size = [end[0] - start[0], end[1] - start[1]];
+                     ctx.fillRect(start[0], start[1], size[0], size[1]);
             }
         });
     }
